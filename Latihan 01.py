df59298b22465aba6a6fe5c7aa507652be4948a8
Vegard25 <tamarovegard@gmail.com>
2022-09-22 10:10:40 +0700

    Create Latihan 01.py

---
+++ Latihan 01.py
@@ -0,0 +1,8 @@
+print("Vegard Sinaga")
+print("program menghitung luas persegi panjang")
+satuan = input("satuan yang ingin di gunakan: ")
+panjang =int(input("masukan nilai panjang: "))
+lebar =int(input("masukan nilai lebar: "))
+luas= panjang*lebar
+print("tampilkan luas persegi panjang =" + str(luas), satuan)
+
