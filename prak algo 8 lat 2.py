336939a7030462ba67c7a768ba049a801155a2cf
Vegard25 <114125017+Vegard25@users.noreply.github.com>
2022-11-15 16:05:04 +0700

    Add files via upload

---
+++ prak algo 8 lat 2.py
@@ -0,0 +1,12 @@
+def pangkat(a,b,c=1):
+    if b>0:
+        c=c*a
+        b=b-1
+        pangkat(a, b, c)
+    else:
+        print(c)
+        
+
+angka=int(input('angka: '))
+x=int(input('pangkat: '))
+pangkat(angka, x)
